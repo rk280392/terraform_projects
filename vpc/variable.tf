280907b790350ae9492316b30be08d65ccc87c1b
root <root@ip-172-31-44-74.ap-south-1.compute.internal>
2020-02-16 18:54:01 +0000

    added rds files

--- vpc/variable.tf
+++ vpc/variable.tf
@@ -5,3 +5,14 @@ variable "VPC_PUBLIC_SUBNET1_CIDR_BLOCK" {}
 variable "VPC_PUBLIC_SUBNET2_CIDR_BLOCK" {}
 variable "VPC_PRIVATE_SUBNET1_CIDR_BLOCK" {}
 variable "VPC_PRIVATE_SUBNET2_CIDR_BLOCK" {}
+
+variable "RDS_CIDR" {}
+variable "DB_INSTANCE_CLASS" {}
+variable "RDS_ENGINE" {}
+variable "ENGINE_VERSION" {}
+variable "BACKUP_RETENTION_PERIOD" {}
+variable "RDS_ALLOCATED_STORAGE" {}
+variable "PUBLICLY_ACCESSIBLE" {}
+variable "RDS_USERNAME" {}
+variable "RDS_PASSWORD" {}
+
